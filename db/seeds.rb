a479a25eadcb25d0ba946092b4229d7e85049d5d
jack glen <jackglen0@gmail.com>
2022-03-29 14:26:49 +0100

    full update on app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+  Restaurant.create(
+    name:    Faker::Company.name,
+    address: Faker::Address.street_address,
+    phone_number:  "07889129929",
+    category: "chinese"
+  )
+end
